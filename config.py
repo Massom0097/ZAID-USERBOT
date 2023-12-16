23554a7e2d37bc5100c9a967a10760030c993e2d
Massom0097 <121150107+Massom0097@users.noreply.github.com>
2023-12-16 19:58:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ if os.path.exists("local.env"):
 
 
 API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_HASH = getenv("API_HASH", "7ce8c14e5bbf55849a45a572ec4e83ed") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
