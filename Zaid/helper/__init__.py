9bd3c18eb9b9129f5dcd135681705f1bed89cc23
Massom0097 <121150107+Massom0097@users.noreply.github.com>
2023-12-16 18:59:24 +0530

    Update __init__.py

--- Zaid/helper/__init__.py
+++ Zaid/helper/__init__.py
@@ -9,6 +9,6 @@ def restart():
 
 async def join(client):
     try:
-        await client.join_chat("TheUpdatesChannel")
+        await client.join_chat("innocent_world_chat")
     except BaseException:
         pass
